73ce24a057a89cfd555d20ba2cfa06077ce81163
jffryan <jffryan@gmail.com>
2022-08-27 22:01:10 -0500

    add table toggle. beginning to see redundancy

--- src/views/GenreView.vue
+++ src/views/GenreView.vue
@@ -2,18 +2,34 @@
   <div class="grid grid-cols-12 gap-2 py-20">
     <basic-hero :title="genreInView" />
     <div class="col-start-2 col-span-10">
-      <div class="pb-4">
+      <div class="pb-4 flex justify-between">
         <router-link :to="{ name: 'books' }">Back to all books</router-link>
+        <div>
+          <button
+            class="pr-2 pb-2 hover:underline"
+            @click="setShelfStyle('card')"
+          >
+            Card
+          </button>
+          <button
+            class="pr-2 pb-2 hover:underline"
+            @click="setShelfStyle('table')"
+          >
+            Table
+          </button>
+        </div>
       </div>
-      <card-shelf />
+      <card-shelf v-if="shelfStyle === 'card'" />
+      <table-shelf v-if="shelfStyle === 'table'" />
     </div>
   </div>
 </template>
 
 <script>
 // @todo: Display genres + merge & refactor with other shelves
-import CardShelf from "@/components/shelves/CardShelf.vue";
 import BasicHero from "@/components/layouts/BasicHero.vue";
+import CardShelf from "@/components/shelves/CardShelf.vue";
+import TableShelf from "@/components/shelves/TableShelf.vue";
 
 import {
   FETCH_BOOKS,
@@ -24,8 +40,14 @@ import {
 export default {
   name: "GenreView",
   components: {
-    CardShelf,
     BasicHero,
+    CardShelf,
+    TableShelf,
+  },
+  data() {
+    return {
+      shelfStyle: "card",
+    };
   },
   computed: {
     genreInView() {
@@ -40,10 +62,16 @@ export default {
   updated() {
     this.updateGenreFilter(this.genreInView);
   },
+  unmounted() {
+    this.updateGenreFilter(null);
+  },
   methods: {
     updateGenreFilter(genre) {
       this.$store.commit(SET_FILTER_BY_GENRE, genre);
       this.$store.commit(FILTER_THROUGH_BOOKS);
+    },
+    setShelfStyle(shelfStyle) {
+      this.shelfStyle = shelfStyle;
     },
   },
 };
